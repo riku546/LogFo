3c00ba8b6580a8f1ba346e85cffb84081007ebf7
riku546 <riku62560@gmail.com>
2025-11-16 21:28:54 +0900

    chore: ciに依存関係のキャッシュを追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,15 +4,15 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+  return c.text("Hello Hono!");
 });
 
 serve(
-	{
-		fetch: app.fetch,
-		port: 8000,
-	},
-	(info) => {
-		console.log(`Server is running on http://localhost:${info.port}`);
-	},
+  {
+    fetch: app.fetch,
+    port: 8000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
 );
