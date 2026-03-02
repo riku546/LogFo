8a7f55b352ae2a6077ddf6fc64d01a887ac260db
riku546 <riku62560@gmail.com>
2026-03-02 10:54:44 +0900

    build(backend): Vitestの設定を修正し、Cloudflare Workersのインテグレーションテストを修正
    を使用してインテグレーションテスト側のプロジェクトを定義することで、
    の解決やマイグレーションの適用が正しく動作するように修正しました。
    
    また、ユニットテスト側は標準のNode.jsスレッドで実行されるよう設定を分離しました。

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -1,28 +1,47 @@
 import path from "node:path";
+import { defineConfig } from "vitest/config";
 import {
   defineWorkersProject,
   readD1Migrations,
 } from "@cloudflare/vitest-pool-workers/config";
 
-export default defineWorkersProject(async () => {
+export default defineConfig(async () => {
   // drizzleディレクトリ内のマイグレーションを読み込む
   const migrationsPath = path.join(__dirname, "drizzle");
   const migrations = await readD1Migrations(migrationsPath);
 
   return {
     test: {
-      setupFiles: ["./test/apply-migrations.ts"],
-      poolOptions: {
-        workers: {
-          wrangler: {
-            configPath: "./wrangler.toml",
-          },
-          miniflare: {
-            // マイグレーションをセットアップファイルで適用できるようにバインディングを追加
-            bindings: { TEST_MIGRATIONS: migrations },
+      projects: [
+        //ユニットテストのテストファイルはnode上で実行する
+        {
+          name: "unit",
+          test: {
+            include: ["test/unit/**/*.test.ts"],
+            environment: "node",
+            pool: "threads",
           },
         },
-      },
+        //統合テストのテストファイルはworkers上で実行する
+        defineWorkersProject({
+          test: {
+            pool: "@cloudflare/vitest-pool-workers",
+            include: ["test/integration/**/*.test.ts"],
+            setupFiles: ["./test/apply-migrations.ts"],
+            poolOptions: {
+              workers: {
+                wrangler: {
+                  configPath: "./wrangler.toml",
+                },
+                miniflare: {
+                  // マイグレーションをセットアップファイルで適用できるようにバインディングを追加
+                  bindings: { TEST_MIGRATIONS: migrations },
+                },
+              },
+            },
+          },
+        }),
+      ],
     },
   };
 });
